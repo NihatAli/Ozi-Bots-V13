84aa7a54e5fc0e623ec782ab0749a92c9c72d0b4
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-08-04 12:38:23 +0300

    Update ozi.js

--- ozi.js
+++ ozi.js
@@ -1,2 +1,3 @@
-Bu bot market sistemi dahil olmak üzere geliştirilip v13 hale sorunsuz getirilmiştir almak isteyenler Discord: Jaylen#1404 den fiyat bilgilendirme alabilirler. 
+Bu bot market sistemi dahil olmak üzere geliştirilip v13 hale sorunsuz getirilmiştir.
+Satın almak isteyenler Discord: Jaylen#9097 den fiyat bilgilendirme alabilirler. 
 S4tıldıktan sonra bu bot asla paylaşılmayacaktır.
