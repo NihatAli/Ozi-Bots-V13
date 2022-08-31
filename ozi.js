9af274b35797ca375bf5e97788a3ff52b367a628
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-08-31 22:15:03 +0300

    Delete ozi.js

--- ozi.js
+++
@@ -1,3 +0,0 @@
-Bu bot market sistemi dahil olmak üzere geliştirilip v13 hale sorunsuz getirilmiştir.
-Satın almak isteyenler Discord: Jaylen#9097 den fiyat bilgilendirme alabilirler. 
-S4tıldıktan sonra bu bot asla paylaşılmayacaktır.
